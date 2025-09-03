a4663df86904ba533baab0b89f270087dfd4f9a6
abdullahaktay <aktay.tr@gmail.com>
2025-09-03 10:08:14 +0300

    feat(api): add TestController with /public endpoint and web starter dependency

---
+++ src/main/java/com/abdullahaktay/jwt/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.abdullahaktay.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "This is a public endpoint";
+    }
+}
