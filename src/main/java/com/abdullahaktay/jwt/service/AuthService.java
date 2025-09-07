bab4217bc53976d2b52bed0d366ac6cf3bce555f
abdullahaktay <aktay.tr@gmail.com>
2025-09-07 16:27:53 +0300

    feat(auth): authenticate login requests with AuthenticationManager

--- src/main/java/com/abdullahaktay/jwt/service/AuthService.java
+++ src/main/java/com/abdullahaktay/jwt/service/AuthService.java
@@ -3,6 +3,9 @@ package com.abdullahaktay.jwt.service;
 import com.abdullahaktay.jwt.config.JwtProperties;
 import com.abdullahaktay.jwt.dto.request.AuthRequest;
 import com.abdullahaktay.jwt.dto.response.AuthResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -16,17 +19,20 @@ public class AuthService {
     private static final String TOKEN_TYPE = "Bearer";
     private final JwtEncoder jwtEncoder;
     private final JwtProperties jwtProperties;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthService(JwtEncoder jwtEncoder, JwtProperties jwtProperties) {
+    public AuthService(JwtEncoder jwtEncoder, JwtProperties jwtProperties, AuthenticationManager authenticationManager) {
         this.jwtEncoder = jwtEncoder;
         this.jwtProperties = jwtProperties;
+        this.authenticationManager = authenticationManager;
     }
 
     public AuthResponse login(AuthRequest authRequest) {
+        Authentication authentication = UsernamePasswordAuthenticationToken.unauthenticated(authRequest.username(), authRequest.password());
+        Authentication authenticationResponse = this.authenticationManager.authenticate(authentication);
         var ttl = jwtProperties.ttl();
-        String token = generateToken(authRequest.username(), ttl);
+        String token = generateToken(authenticationResponse.getName(), ttl);
         return new AuthResponse(token, TOKEN_TYPE, ttl.toSeconds());
-
     }
 
     private String generateToken(String subject, Duration ttl) {
@@ -40,7 +46,5 @@ public class AuthService {
                 .build();
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-
-
     }
 }
