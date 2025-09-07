eb1b8fcb596d225ecf7ced5ea51acf7b5d1acb8d
abdullahaktay <aktay.tr@gmail.com>
2025-09-07 11:18:38 +0300

    feat(security): add JwtEncoder bean using RSA key pair

--- src/main/java/com/abdullahaktay/jwt/config/SecurityConfig.java
+++ src/main/java/com/abdullahaktay/jwt/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.abdullahaktay.jwt.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,11 +14,19 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtProperties jwtProperties;
+
+    public SecurityConfig(JwtProperties rsaKeyConfigProperties) {
+        this.jwtProperties = rsaKeyConfigProperties;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,6 +37,14 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK key = new RSAKey.Builder(jwtProperties.publicKey()).privateKey(jwtProperties.privateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(key));
+        return new NimbusJwtEncoder(jwkSource);
+
     }
 
     @Bean
