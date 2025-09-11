f6ae17a1195362e822725f87903056289f6de3fe
abdullahaktay <aktay.tr@gmail.com>
2025-09-11 06:40:22 +0300

    feat(exception): return ProblemDetail for validation errors in GlobalExceptionHandler

--- src/main/java/com/abdullahaktay/jwt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/abdullahaktay/jwt/exception/GlobalExceptionHandler.java
@@ -1,21 +1,38 @@
 package com.abdullahaktay.jwt.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
+import java.time.Instant;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult()
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "One or more fields are invalid.");
+        problemDetail.setTitle("Validation Failed");
+        problemDetail.setProperty("errorCode", "VALIDATION_001");
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        Map<String, String> fieldErrors = exception.getBindingResult()
                 .getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return errors;
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fe -> Objects.requireNonNullElse(fe.getDefaultMessage(), "Invalid value"),
+                        (msg1, msg2) -> msg1
+                ));
+        problemDetail.setProperty("errors", fieldErrors);
+
+
+        return problemDetail;
     }
 }
