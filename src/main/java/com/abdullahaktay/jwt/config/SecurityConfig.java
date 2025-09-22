ba2271199dec02b33a58bb5b2ef88d2ad3633eb2
abdullahaktay <aktay.tr@gmail.com>
2025-09-22 13:32:27 +0300

    security(password): increase BCrypt strength to 12

--- src/main/java/com/abdullahaktay/jwt/config/SecurityConfig.java
+++ src/main/java/com/abdullahaktay/jwt/config/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
