0bd3dbb40542c2782d6e66acef82c5fa16048bd4
abdullahaktay <aktay.tr@gmail.com>
2025-09-23 14:20:48 +0300

    refactor(entity): update User entity to extend BaseEntity

--- src/main/java/com/abdullahaktay/jwt/domain/entity/User.java
+++ src/main/java/com/abdullahaktay/jwt/domain/entity/User.java
@@ -1,16 +1,12 @@
 package com.abdullahaktay.jwt.domain.entity;
 
-import jakarta.persistence.*;
-
-import java.time.Instant;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String username;
@@ -19,23 +15,4 @@ public class User {
     private String password;
 
     private boolean enabled = true;
-
-    @Column(name = "created_at", updatable = false)
-    private Instant createdAt;
-
-    @Column(name = "updated_at")
-    private Instant updatedAt;
-
-    @Column(name = "deleted_at")
-    private Instant deletedAt;
-
-    @PrePersist
-    void prePersist() {
-        this.createdAt = Instant.now();
-    }
-
-    @PreUpdate
-    void preUpdate() {
-        this.updatedAt = Instant.now();
-    }
 }
