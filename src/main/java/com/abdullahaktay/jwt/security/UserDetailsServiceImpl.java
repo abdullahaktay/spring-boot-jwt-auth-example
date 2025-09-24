d6c188e3736ca6ebac4f2c51b43f74689f67382d
abdullahaktay <aktay.tr@gmail.com>
2025-09-24 10:31:00 +0300

    feat(security): implement JPA-backed UserDetailsService

---
+++ src/main/java/com/abdullahaktay/jwt/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.abdullahaktay.jwt.security;
+
+import com.abdullahaktay.jwt.domain.entity.User;
+import com.abdullahaktay.jwt.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserPrincipal(user);
+
+    }
+}
